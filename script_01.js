be3e155b76635dd80c1b4e281a45d37f516d2a4a
Mia Kamenetski <mia.kamenetski@online.de>
2022-11-11 10:47:13 +0100

    Funktionen

--- script_01.js
+++ script_01.js
@@ -2,3 +2,36 @@
 
 /***** Funktionen 01 *****/
 
+// 1. Kapselung von Codeblöken
+
+// Funktionsaufruf (call)
+// test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+// function test()
+// {
+//     console.log("Hallo Sabine!");
+// }
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// ausgabeNamen();
+// console.log(firstName); // Fehler: Scope!
+
+// function ausgabeNamen() {
+//     let firstName = "Michael";
+//     console.log("Hallo " + firstName + "!");
+// }
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+ausgabeNamenParam(prompt("Please enter a name:","Name")); // Argument
+// Argumente sind Daten für Parameter
+
+function ausgabeNamenParam(firstName) //Parameter
+{
+    console.log("Welcome, " + firstName + "!");
+}
